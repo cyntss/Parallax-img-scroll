c335be31813cddc47f79082cd7c9c85fa7059838
Cynthia Sanchez <csanchez@suse.com>
2014-05-27 16:42:18 +0200

    parallaximg.js: bugs fixed. Index.html no longer needed

--- parallaxImg.js
+++ parallaxImg.js
@@ -19,13 +19,12 @@ $(document).ready(function() {
 });
 
 var parallaxElementsArray = [];
-var sizeOfContainers = 0
 
 /* Initial setup of the elements */
 function initialSetup() {
 	$(".parallax-img-container").each(function() {
-		sizeOfContainers = sizeOfContainers + $(".parallax-img-container").width();
-	  var setOfElements = $(".parallax-img-container").children();
+		var sizeOfContainer = $(this).width();
+	  var setOfElements = $(this).children();
     for (i = 0; i < setOfElements.length ; i++) {
 			var classApplied = $(setOfElements[i]).attr('class');	
 			if (classApplied != "parallax-move") {
@@ -35,16 +34,17 @@ function initialSetup() {
 				})
 			} else {
 				$(setOfElements[i]).css({
-					bottom: "-100px"
+					bottom: "-100px",
+					"opacity" : 0
 				})
-				var ranNumSpeed = Math.floor((Math.random() * 20)+1);
+				var ranNumSpeed = Math.floor((Math.random() * 25)+1);
 				if(ranNumSpeed < 10) {
 				  var scrollSpeed = "0.0"+ranNumSpeed;	
 				} else {
 					var scrollSpeed = "0."+ranNumSpeed;
 				}
 	  	  
-	  	  var ranNumPosition = Math.floor(Math.random() * (sizeOfContainers - 25 + 1)) + 25;
+	  	  var ranNumPosition = Math.floor(Math.random() * (sizeOfContainer - 200) + 50);
 				parallaxElementsArray.push({
 					"element": $(setOfElements[i]),
 					"scrollSpeed": scrollSpeed,
@@ -64,13 +64,28 @@ function initialSetup() {
 	});
 }
 
+var alpha = 0;
+var lastestScrolled = 0;
+var scrolled = 0;
+
 /* Move the images while scrolling the page */
 function parallaxImgScroll(){
-  var scrolled = $(window).scrollTop();
+  scrolled = $(window).scrollTop();
+  if (lastestScrolled < scrolled) {
+    alpha = alpha + 0.02;	
+  } else if (scrolled == 0) {
+  	alpha = 0;
+  } else {
+  	alpha = alpha - 0.02;
+  }
+  console.log("last: "+lastestScrolled+ " new: "+scrolled + " alpha: "+alpha)
+  
   for (i = 0; i < parallaxElementsArray.length; i++ ) {
     $(parallaxElementsArray[i].element).css({
     	'bottom': (1+(scrolled*parallaxElementsArray[i].scrollSpeed))+'em',
-    	'left': parallaxElementsArray[i].pagePosition
+    	'left': parallaxElementsArray[i].pagePosition,
+    	"opacity" : alpha
     	});
   }
+  lastestScrolled = scrolled;
 }
