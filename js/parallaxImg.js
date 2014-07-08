962a5385d1a421f4e7aa3dab592fa1a192166301
Cynthia Sanchez <csanchez@suse.com>
2014-07-08 12:55:55 +0200

    V.1.2.1 adds configuration variables to be sent on Document Ready from HTML to set the initial opacity as well as the speed of opacity

--- js/parallaxImg.js
+++ js/parallaxImg.js
@@ -8,140 +8,157 @@
 ╱╱╱╭━╯┃///////////////////
 ╱╱╱╰━━╯http://cynt.co.nf/
 ////////////////////////
-V.1.2.0
+V.1.2.1
 */
-$(document).ready(function (){
-  $(".parallax-move").css({
-    'opacity' : 0,
-    position: "absolute",
-    "z-index": 1
-  });
-})
 
-$(window).load(function() {
-  initialSetup();
-  /* Scroll event to trigger the function */
-  $(window).bind('scroll',function(e){
-    parallaxImgScroll(); 
-  });
-});
+function parallaxImgScroll(settings) {
 
-var parallaxElementsArray = [];
-var alpha = 0;
-var lastestScrolled = 0;
-var scrolled = 0;
+  //if the user is setting the configuration
+  var default_settings = {
+    initialOpacity : 0,
+    opacitySpeed : 0.02 //values from 0.01 to 1 -> 0.01: slowly appears on screen; 1: appears as soon as the user scrolls 1px
+  }
+  var parallaxSettings = $.extend({}, default_settings, settings);
 
-/* Initial setup of the elements */
-function initialSetup() {
-  $(".parallax-img-container").each(function() {
-    var widthOfContainer = $(this).width();
-    var heightOfContainer = $(this).height();
-    var setOfElements = $(this).children();
-    for (i = 0; i < setOfElements.length ; i++) {
-      var classApplied = $(setOfElements[i]).attr('class');
-      if (classApplied != "parallax-move") {
-        $(setOfElements[i]).css({
-          "z-index": 100,
-          "position": "relative"
-        })
-      } else {
-        var ranNumSpeed = Math.floor((Math.random() * 100) + 30);
-        if(ranNumSpeed < 10) {
-          var scrollSpeed = "0.0" + ranNumSpeed;  
-        } else {
-          var scrollSpeed = "0." + ranNumSpeed;
-        }
+  //definition of essential variables [do not modify]
+  var parallaxElementsArray = [];
+  var lastestScrolled = 0;
+  var scrolled = 0;
 
-        if ($(setOfElements[i]).hasData('vertical-position')) {
-          ranNumTopPosition = $(setOfElements[i]).data('vertical-position');
-        } else {
-          var ranNumTopPosition = Math.floor(Math.random() * (heightOfContainer - (heightOfContainer/4)) + 1);  
-        }
-        
-        if ($(setOfElements[i]).hasData('horizontal-position')) {
-          var ranNumBottomPosition = $(setOfElements[i]).data('horizontal-position');
-        } else {
-          var ranNumBottomPosition = Math.floor(Math.random() * (widthOfContainer - 200) + 50);  
-        }
-        
-        parallaxElementsArray.push({
-          "element" : $(setOfElements[i]),
-          "scrollSpeed" : scrollSpeed,
-          "horizontalPagePosition" : ranNumBottomPosition,
-          "verticalPagePosition" : ranNumTopPosition,
-          "opacity" : alpha
-        });
-
-        /* Apply initial position */
-        $(setOfElements[i]).css({
-          "bottom": ranNumTopPosition,
-          "left": ranNumBottomPosition
-        })
-      }
-    }
+  $(document).ready(function (){
+    $(".parallax-move").css({
+      'opacity' : 0,
+      position: "absolute",
+      "z-index": 1
+    });
   })
 
-  $(".parallax-img-container").css({
-    position: "relative",
-    overflow: "hidden"
+  $(window).load(function() {
+    $(".parallax-move").css({
+      'opacity' : parallaxSettings.initialOpacity
+    });
+    parallaxImgInit();
+    /* Scroll event to trigger the function */
+    $(window).bind('scroll',function(e){
+      parallaxImgScroll(); 
+    });
   });
 
-}
+  /* Initial setup of the elements */
+  function parallaxImgInit() {
 
-/* Move the images while scrolling the page */
-function parallaxImgScroll(){
+    $(".parallax-img-container").each(function() {
+      var widthOfContainer = $(this).width();
+      var heightOfContainer = $(this).height();
+      var setOfElements = $(this).children();
+      for (i = 0; i < setOfElements.length ; i++) {
+        var classApplied = $(setOfElements[i]).attr('class');
+        if (classApplied != "parallax-move") {
+          $(setOfElements[i]).css({
+            "z-index": 100,
+            "position": "relative"
+          })
+        } else {
+          var ranNumSpeed = Math.floor((Math.random() * 100) + 30);
+          if(ranNumSpeed < 10) {
+            var scrollSpeed = "0.0" + ranNumSpeed;  
+          } else {
+            var scrollSpeed = "0." + ranNumSpeed;
+          }
 
-  for (i = 0; i < parallaxElementsArray.length; i++) {
-    
-    scrolled = $(window).scrollTop();
-    alpha = parallaxElementsArray[i].opacity;
-    /* Calculate the distance between the element and the top of the document */
-    var distanceFromTop = $(parallaxElementsArray[i].element).offset().top;
+          if ($(setOfElements[i]).hasData('vertical-position')) {
+            ranNumTopPosition = $(setOfElements[i]).data('vertical-position');
+          } else {
+            var ranNumTopPosition = Math.floor(Math.random() * (heightOfContainer - (heightOfContainer/4)) + 1);  
+          }
+          
+          if ($(setOfElements[i]).hasData('horizontal-position')) {
+            var ranNumBottomPosition = $(setOfElements[i]).data('horizontal-position');
+          } else {
+            var ranNumBottomPosition = Math.floor(Math.random() * (widthOfContainer - 200) + 50);  
+          }
+          
+          parallaxElementsArray.push({
+            "element" : $(setOfElements[i]),
+            "scrollSpeed" : scrollSpeed,
+            "horizontalPagePosition" : ranNumBottomPosition,
+            "verticalPagePosition" : ranNumTopPosition,
+            "opacity" : parallaxSettings.initialOpacity
+          });
 
-    if (isVisible(distanceFromTop)) {
-      /* make the element appear progressively */
-      /* if scrolling down */
-      if (lastestScrolled < scrolled) {
-        alpha = alpha + opacitySpeed;
-        if (alpha > 1) {
-          alpha = 1;
-        }
-      } else if (scrolled == 0) {
-        alpha = 0;
-      /* else.. if scrolling up */
-      } else {
-        alpha = alpha - opacitySpeed;
-        if (alpha < 0) {
-          alpha = 0;
+          /* Apply initial position */
+          $(setOfElements[i]).css({
+            "bottom": ranNumTopPosition,
+            "left": ranNumBottomPosition
+          })
         }
       }
-      $(parallaxElementsArray[i].element).css({
-        'bottom': (parallaxElementsArray[i].verticalPagePosition + (scrolled * parallaxElementsArray[i].scrollSpeed))+'px',
-        "opacity" : alpha
-        });
+    })
 
-      /* save the opacity in the elements object */
-      parallaxElementsArray[i].opacity = alpha
-    }      
+    $(".parallax-img-container").css({
+      position: "relative",
+      overflow: "hidden"
+    });
+
   }
-  lastestScrolled = scrolled;
 
-  /* check if the element is visible on screen */
-  function isVisible(distance) {
-    if (distance < scrolled) {
-      return false;
-    } else if ([scrolled + $(window).height()] < distance) {
-      return false;
-    } else {
-      return true;
+  /* Move the images while scrolling the page */
+  function parallaxImgScroll(){
+
+    for (i = 0; i < parallaxElementsArray.length; i++) {
+      
+      scrolled = $(window).scrollTop();
+      alpha = parallaxElementsArray[i].opacity;
+      /* Calculate the distance between the element and the top of the document */
+      var distanceFromTop = $(parallaxElementsArray[i].element).offset().top;
+
+      if (isVisible(distanceFromTop)) {
+        /* unless parallaxSettings.opacitySpeed = 1, make the element appear progressively */
+        if (parallaxSettings.initialOpacity != 1) {
+          /* if scrolling down */
+          if (lastestScrolled < scrolled) {
+            alpha = alpha + parallaxSettings.opacitySpeed;
+            if (alpha > 1) {
+              alpha = 1;
+            }
+          } else if (scrolled == 0) {
+            alpha = parallaxSettings.initialOpacity;
+          /* else.. if scrolling up */
+          } else {
+            alpha = alpha - parallaxSettings.opacitySpeed;
+            if (alpha < parallaxSettings.initialOpacity) {
+              alpha = parallaxSettings.initialOpacity;
+            }
+          }
+        } else {
+          alpha = parallaxSettings.initialOpacity;
+        }
+        $(parallaxElementsArray[i].element).css({
+          'bottom': (parallaxElementsArray[i].verticalPagePosition + (scrolled * parallaxElementsArray[i].scrollSpeed))+'px',
+          "opacity" : alpha
+          });
+
+        /* save the opacity in the elements object */
+        parallaxElementsArray[i].opacity = alpha;
+      }      
+    }
+    lastestScrolled = scrolled;
+
+    /* check if the element is visible on screen */
+    function isVisible(distance) {
+      if (distance < scrolled) {
+        return false;
+      } else if ([scrolled + $(window).height()] < distance) {
+        return false;
+      } else {
+        return true;
+      }
     }
   }
 
+  /* check if a data attribute exists */
+  $.fn.hasData = function(attrName) {
+    return (typeof $(this).data(attrName) != 'undefined');
+  };
+
 }
-
-/* check if a data attribute exists */
-$.fn.hasData = function(attrName) {
-  return (typeof $(this).data(attrName) != 'undefined');
-};
-
-
